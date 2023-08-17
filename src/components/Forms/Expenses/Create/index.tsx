5e16ad4ee80c2108ee66b863365f1c9f1d11600a
mateus-werneck <werneck.mateus@gmail.com>
2023-08-17 18:45:43 -0300

    Confirm Alert

--- src/components/Forms/Expenses/Create/index.tsx
+++ src/components/Forms/Expenses/Create/index.tsx
@@ -1,8 +1,12 @@
 'use client';
 import { StandardForm } from '@Components/Structure/Form';
 import { IStandardInput } from '@Components/Structure/Form/Input/types';
+import { MoneyUtil } from '@Lib/Treat/Money';
 import { ZodValidator } from '@Lib/Validators/Zod';
+import { CreateExpense } from '@Types/Expense/types';
 import { ExpenseType } from '@prisma/client';
+import { confirmAlert } from 'react-confirm-alert';
+import { UseFormReturn } from 'react-hook-form';
 import { z } from 'zod';
 
 interface IExpenseForm {
@@ -19,20 +23,33 @@ interface IExpenseSubmit {
 export default function ExpenseForm({ expenseTypes }: IExpenseForm) {
   const { inputs, validationSchema } = useFormData(expenseTypes);
 
-  const onFormSubmit = (data: any) => {
-    console.log(data);
-    // const expense: CreateExpense = {
-    //   name: data.name,
-    //   value: MoneyUtil.toFloat(data.value),
-    //   dueDate: new Date(data.dueDate),
-    //   type: data.expenseType.id
-    // };
-    // fetch(`${process.env.NEXT_PUBLIC_SYSTEM_URL}/api/expenses`, {
-    //   method: 'POST',
-    //   body: JSON.stringify(expense)
-    // })
-    //   .then((value) => value.json().then((response) => console.log(response)))
-    //   .catch((reason) => console.log(reason));
+  const onFormSubmit = (
+    data: IExpenseSubmit,
+    formContext: UseFormReturn<any>
+  ) => {
+    const expense: CreateExpense = {
+      name: data.name,
+      value: MoneyUtil.toFloat(data.value),
+      dueDate: new Date(data.dueDate),
+      type: data.expenseType.id
+    };
+    fetch(`${process.env.NEXT_PUBLIC_SYSTEM_URL}/api/expens2des`, {
+      method: 'POST',
+      body: JSON.stringify(expense)
+    })
+      .then((value) => value.json().then((response) => console.log(response)))
+      .catch((reason: any) => {
+        confirmAlert({
+          title: 'Error ao realizar cadastro',
+          message: 'Verifique os dados enviados.',
+          buttons: [
+            {
+              label: 'Ok',
+              onClick: () => formContext.reset()
+            }
+          ]
+        });
+      });
   };
 
   return (
