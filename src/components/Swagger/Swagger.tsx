00dd9c48471d0f9954511ff8f02bdb838dff6382
mateus-werneck <werneck.mateus@gmail.com>
2023-09-02 15:47:41 -0300

    Ajustes

--- src/components/Swagger/Swagger.tsx
+++ src/components/Swagger/Swagger.tsx
@@ -3,6 +3,6 @@ import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
 export default function Swagger() {
-  const systemUrl = process.env.NEXT_PUBLIC_SYSTEM_URL;
+  const systemUrl = window.location.protocol + '//' + window.location.host;
   return <SwaggerUI url={`${systemUrl}/docs/swagger.yaml`} deepLinking />;
 }
