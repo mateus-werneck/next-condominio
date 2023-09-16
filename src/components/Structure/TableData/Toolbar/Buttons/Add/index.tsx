5b64ccf665c7f334662258645c79711dbd3102ca
mateus-werneck <werneck.mateus@gmail.com>
2023-09-16 10:21:27 -0300

    Ajustes Botao

--- src/components/Structure/TableData/Toolbar/Buttons/Add/index.tsx
+++ src/components/Structure/TableData/Toolbar/Buttons/Add/index.tsx
@@ -1,10 +1,9 @@
 import Button from '@Components/Structure/Button';
-import { Colors } from '@Lib/Treat/Colors';
 import AddIcon from '@mui/icons-material/Add';
 
 export default function Add(route: string): JSX.Element[] {
   return [
-    <Button route={route} variant="outlined" key={route} color={Colors.success}>
+    <Button route={route} variant="text" key={route}>
       <AddIcon fontSize="small" />
       Adicionar
     </Button>
