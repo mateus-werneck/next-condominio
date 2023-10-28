571c3341d7ef3c83820a98cd1e7172ca38ea6cc2
mateus-werneck <werneck.mateus@gmail.com>
2023-10-28 11:07:09 -0300

    Export Icons

--- src/components/Structure/TableData/Helpers/Export/Export.tsx
+++ src/components/Structure/TableData/Helpers/Export/Export.tsx
@@ -1,14 +1,25 @@
+import CustomIcon from '@Components/Structure/CustomIcon';
 import { GridToolbarExportContainer } from '@mui/x-data-grid';
 
 export default function Export({ rows }: { rows: any[] }) {
+  const styles =
+    'flex w-full justify-start items-center font-normal leading-6 bg-transparent indent-4 hover:bg-black/[.04] min-w-[80px] min-h-[40px]';
   const ExportExcel = () => (
-    <button className=" flex justify-start items-center font-normal leading-6 bg-transparent indent-4 hover:bg-black/[.04] min-w-[80px] min-h-[40px]">
-      Excel
+    <button
+      className={styles}
+      onClick={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <div className="flex p-4">
+        <CustomIcon src="XLSX" alt="EXPORT_XLSX_ICON" />
+        EXCEL
+      </div>
     </button>
   );
   const ExportJson = () => (
     <button
-      className=" flex justify-start items-center font-normal leading-6 bg-transparent indent-4 hover:bg-black/[.04] min-w-[80px] min-h-[40px]"
+      className={styles}
       onClick={(e) => {
         e.preventDefault();
         const content = JSON.stringify(rows);
@@ -20,7 +31,10 @@ export default function Export({ rows }: { rows: any[] }) {
         ancor.click();
       }}
     >
-      JSON
+      <div className="flex p-4">
+        <CustomIcon src="JSON" alt="EXPORT_JSON_ICON" />
+        JSON
+      </div>
     </button>
   );
 
