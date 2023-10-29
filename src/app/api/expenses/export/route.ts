1fc95ff40932ba1dadd06b7c9a265bf3b15addbf
mateus-werneck <werneck.mateus@gmail.com>
2023-10-29 10:29:50 -0300

    ExcelExpot

--- src/app/api/expenses/export/route.ts
+++ src/app/api/expenses/export/route.ts
@@ -1,12 +1,24 @@
 import { ExpenseMap } from '@Lib/Data/Expense/export';
-import { exportSheet } from '@Lib/Export/ExportFile';
+import { exportSheet } from '@Lib/Export/ExcelExport';
+import { DateUtil } from '@Lib/Treat/Date';
+import { MoneyUtil } from '@Lib/Treat/Money';
 import { ExpenseDto } from '@Types/Expense/types';
+import { Row } from 'exceljs';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
   const rows = await request.json();
   const data = rows.map(mapEachExpense);
-  const file = await exportSheet(data);
+
+  const applyFormatting = (row: Row) => {
+    const amount = row.getCell('B');
+    amount.numFmt = '"R$"#.##';
+
+    const dueDate = row.getCell('C');
+    dueDate.numFmt = 'dd/mm/yyyy';
+  };
+
+  const file = await exportSheet(data, applyFormatting);
 
   return NextResponse.json({ file });
 }
@@ -23,6 +35,16 @@ function mapEachExpense(entity: Record<string, any>) {
     }
 
     accumulator[header] = entity[propertyName as keyof ExpenseDto];
+
+    if (propertyName === 'value') {
+      accumulator[header] = MoneyUtil.toFloat(accumulator[header]);
+    }
+
+    if (propertyName === 'dueDate') {
+      accumulator[header] = DateUtil.toDateObject(
+        DateUtil.toISOString(accumulator[header])
+      );
+    }
 
     return accumulator;
   }, {});
---
+++ src/lib/Export/ExcelExport.ts
@@ -0,0 +1,65 @@
+import { SheetRecord } from '@Lib/Treat/Excel';
+import { Alignment, Borders, Font, Workbook, Worksheet, Row } from 'exceljs';
+
+export async function exportSheet<T extends Record<string, any>>(
+  data: T[],
+  applyFormatting?: (row: Row) => void
+): Promise<string> {
+  const workbook = new Workbook();
+
+  const worksheet = workbook.addWorksheet('Despesas', {
+    properties: {
+      defaultColWidth: 25
+    }
+  });
+
+  const headerRow = worksheet.addRow(Object.keys(data[0]));
+  const { font, border, alignment } = getDefaultFormat();
+
+  headerRow.eachCell((cell) => {
+    cell.font = { ...font, bold: true };
+    cell.border = border;
+    cell.alignment = alignment;
+  });
+
+  data.forEach((d: SheetRecord) => {
+    const row = getEachRow(d, worksheet);
+    if (applyFormatting) applyFormatting(row);
+  });
+
+  const content = await workbook.xlsx.writeBuffer();
+  const buffer = Buffer.from(content);
+  return buffer.toString('base64');
+}
+
+export function getDefaultFormat() {
+  const font: Partial<Font> = {
+    color: { argb: '#000000' },
+    size: 12
+  };
+
+  const border: Partial<Borders> = {
+    top: { style: 'thin' },
+    left: { style: 'thin' },
+    bottom: { style: 'thin' },
+    right: { style: 'thin' }
+  };
+
+  const alignment: Partial<Alignment> = {
+    vertical: 'middle',
+    horizontal: 'center'
+  };
+
+  return { font, border, alignment };
+}
+
+function getEachRow(d: SheetRecord, worksheet: Worksheet): Row {
+  const row = worksheet.addRow(Object.values(d));
+  const { font, border, alignment } = getDefaultFormat();
+
+  row.font = font;
+  row.border = border;
+  row.alignment = alignment;
+
+  return row;
+}
